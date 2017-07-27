a87294441592faf7984d48f6116f60556d3f0926
Lee Hodge <Lee@LeeHodge.com>
2017-07-27 13:33:44 -0400

    Update html.tpl.php
    Added DAP tracking - Adobe analytics.  First script tag in head and last script tag in body

--- modules/system/html.tpl.php
+++ modules/system/html.tpl.php
@@ -47,6 +47,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
+  <script src="//assets.adobedtm.com/f1bfa9f7170c81b1a9a9ecdcc6c5215ee0b03c84/satelliteLib-4b219b82c4737db0e1797b6c511cf10c802c95cb.js"></script>
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
@@ -59,5 +60,6 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  <script type="text/javascript">_satellite.pageBottom();</script>
 </body>
 </html>
